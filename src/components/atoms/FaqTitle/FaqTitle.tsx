120c7a6a8a36168db3a4bca471a624c6ad242cbb
victor <ejioguvictor6@gmail.com>
2026-01-02 21:12:17 +0100

    added: faq section

---
+++ src/components/atoms/FaqTitle/FaqTitle.tsx
@@ -0,0 +1,11 @@
+import SectionTitleWrapper from "../SectionTitleWrapper/SectionTitleWrapper";
+
+const FaqTitle = () => {
+  return (
+    <SectionTitleWrapper>
+      <span className="text-[#009933]">Frequently</span> asked question
+    </SectionTitleWrapper>
+  );
+};
+
+export default FaqTitle;
