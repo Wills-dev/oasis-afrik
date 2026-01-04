7c771238c4480e159826d20057c584935fc10655
victor <ejioguvictor6@gmail.com>
2026-01-04 18:32:23 +0100

    added: login

--- src/components/molecules/AuthPanelButtons/AuthPanelButtons.tsx
+++ src/components/molecules/AuthPanelButtons/AuthPanelButtons.tsx
@@ -1,21 +1,35 @@
+"use client";
+
 import Link from "next/link";
 
 import Button from "@/components/atoms/Button/Button";
 
+import { useCurrentUser } from "@/features/auth/hooks/useCurrentUser";
+import { useSelector } from "react-redux";
+import { RootState } from "@/store";
+
 const AuthPanelButtons = () => {
+  useCurrentUser();
+  const { user } = useSelector((state: RootState) => state.auth);
+
   return (
     <div className="flex lg:items-center max-lg:flex-col lg:gap-2 gap-6">
-      <Link
-        className="font-medium text-sm hover:text-green-500 transition-all duration-300 cursor-pointer"
-        href="/login"
-      >
-        Login
-      </Link>
-      <Link className="" href="/sign-up">
-        <Button width="w-fit" height="h-[45px]">
-          Join now
-        </Button>
-      </Link>
+      {user ? (
+        <Button href="/dashboard/overview">Dashboard</Button>
+      ) : (
+        <>
+          <Link
+            className="font-medium text-sm hover:text-green-500 transition-all duration-300 cursor-pointer"
+            href="/login"
+          >
+            Login
+          </Link>
+
+          <Button href="/sign-up" width="w-fit" height="h-[45px]">
+            Join now
+          </Button>
+        </>
+      )}
     </div>
   );
 };
